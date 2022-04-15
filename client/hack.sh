d5cd391977253b646c1bd07cb752d0776f80d36f
XuhengLi <xl2784@columbia.edu>
2022-04-15 19:39:00 -0400

    update scripts

--- client/hack.sh
+++ client/hack.sh
@@ -1,4 +1,8 @@
 #!/bin/bash                                                                                                                                                                       
 
 SRV=$1
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; ./hackbench.sh"
+for i in `seq 1 4`; do
+       ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; ./hackbench.sh 20"
+       echo "Grab results for $SRV" 
+       ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; cat hackbench.txt; rm hackbench.txt" >> hackbench.txt
+done
