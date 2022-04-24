a336aa7c253aea54eb83d3206f3b4da058312dc3
XuhengLi <42981800+XuhengLi@users.noreply.github.com>
2022-04-24 18:11:06 -0400

    Update grab-kern.sh

--- client/grab-kern.sh
+++ client/grab-kern.sh
@@ -4,4 +4,4 @@ SRV=$1
 
 echo "Grab results for $SRV" 
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; cat kernbench.txt; rm kernbench.txt" >> kernbench.txt
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; cat kernbench.txt; rm kernbench.txt" >> kern.txt
