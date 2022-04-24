67d42359a44cb909e276cbd3a05ffa6278ce13ac
XuhengLi <42981800+XuhengLi@users.noreply.github.com>
2022-04-24 19:04:56 -0400

    Update kern.sh

--- client/kern.sh
+++ client/kern.sh
@@ -6,5 +6,5 @@ SRV=$1
 for i in `seq 1 4`; do
 	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; ./kernbench.sh"
 	echo "Grab results for $SRV"
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; cat kernbench.txt; rm kernbench.txt" >> kernbench.txt
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; cat kernbench.txt; rm kernbench.txt" >> kern.txt
 done
