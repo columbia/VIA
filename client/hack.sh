b72399af1eb526f23cf6f9bd4ae243d99b21e51c
XuhengLi <42981800+XuhengLi@users.noreply.github.com>
2022-04-24 19:05:13 -0400

    Update hack.sh

--- client/hack.sh
+++ client/hack.sh
@@ -4,5 +4,5 @@ SRV=$1
 for i in `seq 1 4`; do
        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; ./hackbench.sh 20"
        echo "Grab results for $SRV" 
-       ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; cat hackbench.txt; rm hackbench.txt" >> hackbench.txt
+       ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SRV "cd kvmperf/localtests; cat hackbench.txt; rm hackbench.txt" >> hack.txt
 done
