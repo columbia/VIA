056bb08fd791d2a4e823f2c49481418d70bc717f
XuhengLi <42981800+XuhengLi@users.noreply.github.com>
2022-04-24 20:15:15 -0400

    Update install.sh

--- client/install.sh
+++ client/install.sh
@@ -3,6 +3,7 @@
 echo "Downloading YCSB"
 curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
 tar xfvz ycsb-0.17.0.tar.gz
+sed -i '1 s_$_2_' ycsb-0.17.0/bin/ycsb
 
 sleep 1
 echo "Downloading memtier_benchmark"
